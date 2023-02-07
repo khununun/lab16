5d47fb163d99645fbcb3d625426965facead2347
khununun <khununun2546@gmail.com>
2023-02-07 16:11:14 +0700

    Done

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,24 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	int A[4];
+	int B[] = {a,b,c,d};
+	for(int i=0;i<4;i++)
+	{
+		A[i] = rand()%4 ;
+		for(int j=0;j<i;j++)
+		{
+			if(A[i] == A[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+	a = B[A[0]];
+	b = B[A[1]];
+	c = B[A[2]];
+	d = B[A[3]];
+}
